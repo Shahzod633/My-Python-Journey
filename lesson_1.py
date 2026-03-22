e019d52ca7ffd200a8266cacb1dcbd6149c5ede8
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-03-23 01:04:00 +0500

    Add first Python program for learning purposes

---
+++ lesson_1.py
@@ -0,0 +1,9 @@
+# Это моя первая учебная программа
+print("Привет! Я начинаю изучать Python.")
+
+# Давай немного посчитаем
+days_in_year = 365
+hours_in_day = 24
+
+total_hours = days_in_year * hours_in_day
+print(f"В одном году примерно {total_hours} часов!")
