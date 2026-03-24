6dfde458e697e1138ab28fde4be51892beb7aebc
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-03-24 21:41:30 +0500

    Add comments and placeholder for min/max calculation in lesson_3.py

--- lesson_3.py
+++ lesson_3.py
@@ -8,6 +8,16 @@ print(b)
 b=((a*10)%10)/10 
 print(b)'''
 
+# Наибольшее и наименьшее
+''' a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+mi=min(a,b,c,d,e)
+ma=max(a,b,c,d,e)
+print('Наименьшее число', '=', mi)
+print('Наибольшее число =', ma) '''
 
 
 
