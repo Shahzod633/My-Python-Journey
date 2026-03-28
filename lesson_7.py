61578a671d4b9497140e0e305cd2bed13c311bea
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-03-28 19:06:38 +0500

    Add lesson_7.py for string methods and user input greeting

---
+++ lesson_7.py
@@ -0,0 +1,12 @@
+# Тема string  и  методы строк
+'''s='"Python is a great language!"'
+b=', said Fred.'
+d='"I '
+h="don't"
+c=' ever remember having this much fun before."'
+print(str (s)+str (b)+str (d)+str (h)+str (c))'''
+
+# What's Your Name? тема string  и  методы строк
+a=str(input())
+b=str(input())
+print('Hello', a, b + "! You have just delved into Python")
