4c656c3685e39ab48115ac3de10f97b210bf1e0f
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-03-29 21:18:09 +0500

    Comment out the section for calculating Euclidean distance in Lesson_8.py

--- Lesson_8.py
+++ Lesson_8.py
@@ -13,6 +13,19 @@ ca=ceil(a)
 fa=floor(a)
 print(ca+fa)
 '''
+# Евклидово расстояние 
+'''from math import sqrt, pow
+x1=float(input())
+y1=float(input())
+x2=float(input())
+y2=float(input())
+p=sqrt(pow((x1-x2), 2) + pow((y1-y2), 2))
+print(p)
+'''
+
+
+
+
 
 
 
