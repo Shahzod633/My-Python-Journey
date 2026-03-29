7f52a80d4055ce3970d42eec16ae2ddc9b39ad18
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-03-29 21:33:55 +0500

    Add commented section for calculating area of a regular polygon in Lesson_8.py

--- Lesson_8.py
+++ Lesson_8.py
@@ -29,6 +29,18 @@ M=sin(x)+cos(x)+pow(tan(x), 2)
 print(M)
 '''
 
+# Правильный многоугольник
+'''from math import pow, pi, tan
+n=int(input())
+a=float(input())
+S=((n*pow(a, 2))/(4*tan(pi/n)))
+print(S)
+'''
+
+
+
+
+
 
 
 
