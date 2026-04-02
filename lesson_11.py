836d5a9408807524e70be18f2e15bb430e31add9
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-04-02 23:56:13 +0500

    Add lesson_11.py for calculating squares of numbers

---
+++ lesson_11.py
@@ -0,0 +1,6 @@
+# Квадрат числа
+'''from math import sqrt, pow
+a=int(input())
+print('Квадрат числа', 0, "равен", 0 )
+for j in range(a):
+    print('Квадрат числа', j+1, "равен", int(pow(j+1,2)))'''
