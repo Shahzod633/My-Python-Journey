6423a3a86b6902f5652fefdc1b01934c8cf48935
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-04-03 00:27:27 +0500

    Refactor lesson_11.py by commenting out unused code and improving structure

--- lesson_11.py
+++ lesson_11.py
@@ -6,11 +6,18 @@ for j in range(a):
     print('Квадрат числа', j+1, "равен", int(pow(j+1,2)))'''
  
  #Звёздный треугольник 
-a=int(input())
+'''a=int(input())
 for j in range(a+1):
     if a>j:
         b=a-j
         print('*' * b)
+'''
+# Популяция 
+a=int(input())
+b=int(input())
+c=int(input())
+for j in range(c):
+    print(j+1, (a * (b / 100 + 1) ** j) )
 
 
 
