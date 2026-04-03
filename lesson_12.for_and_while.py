4fa884b5f1b10fe739d72cba4ba3743e41134882
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-04-03 23:31:38 +0500

    Add lesson_12.py for population calculation and number sequence examples

---
+++ lesson_12.for_and_while.py
@@ -0,0 +1,27 @@
+# Популяция 
+'''a=int(input())
+b=int(input())
+c=int(input())
+for j in range(c):
+    print(j+1, (a * (b / 100 + 1) ** j) )'''
+
+# продолжение изучение команд for 
+'''for j in range(1, 10):
+    print(j)'''
+
+# таблица умножение 
+'''a=int(input())
+for j in range(1, 11):
+    print(a, 'x', j, '=', a*j )
+'''
+
+# Последовательность чисел 2 
+a=int(input())
+b=int(input())
+for j in range(a, b+1):
+    if j%10==9:
+        print(j)
+    elif  j%15==0:
+        print(j)
+    elif j%17==0:
+        print(j)
