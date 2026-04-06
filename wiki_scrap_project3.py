c8618df890b38921d0f1c86f2fb65aee5f4a1f7e
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-04-06 23:09:46 +0500

    Refactor wiki_scrap_project3.py to enhance web scraping functionality by extracting all h2 sections and printing their titles

--- wiki_scrap_project3.py
+++ wiki_scrap_project3.py
@@ -1,21 +1,13 @@
-import requests
-from bs4 import BeautifulSoup
-
-# 1. Ссылка на статью, которую ты открыл
-url = "https://en.wikipedia.org/wiki/Tajikistan"
-
-# 2. Просим Python "сходить" на сайт и скачать его содержимое
-# Мы добавляем 'headers', чтобы сайт думал, что зашел человек через браузер
-headers = {'User-Agent': 'Mozilla/5.0'}
-response = requests.get(url, headers=headers)
-
-# 3. Превращаем текст страницы в понятный "суп" из тегов
-soup = BeautifulSoup(response.text, 'html.parser')
-
-# 4. САМЫЙ ВАЖНЫЙ МОМЕНТ:
-# Мы говорим: "Найди тег h1, у которого id равен firstHeading"
-# Помнишь, ты это нашел в инспекторе?
-heading = soup.find('h1', id='firstHeading')
-
-# 5. Выводим только текст заголовка, без лишнего кода
-print("Заголовок статьи:", heading.text)
+import requests                                         # мы импортируем библитеку который даёт запрос на сайт 
+from bs4 import BeautifulSoup                           # Этот раздел я не понял!!! 
+url= "https://en.wikipedia.org/wiki/Tajikistan"         # здесь я указал ссылку который в долнешем будт проверять или анализировать 
+headers = {'User-Agent': 'Mozilla/5.0'}                 # А здесь я использую загаловок это как Python не сам ишеет а через браузер mozillsa 5.0
+response=requests.get(url, headers=headers)             # А это ответ от сайта отклик 
+print(f'статус ответа {response.status_code}')          # здесь после придедушей команды он провкряет статус ответа от сайта или ответ отрецателтьный или положительный
+soup=BeautifulSoup(response.text,'html.parser')         # после положительного ответа эмм тоже не понял!!!
+sections=soup.find_all('h2')                            # А здесь команда sections ишет все раздели кода HTML в котором есть код h2 и вводит из всех потомучто я написал команду find.all
+print(f'Найдено разделов: {len(sections)}')             # Выводит все найденнные разделы и их количество 
+print("-" * 20)                                         # не понял
+for section in sections:                                # А это цыкл который связанно со строкой 7 sections - он делает цыкличный запрос на сайт или ишеет все найденный h2 
+    title=section.text.strip()                          #не понял
+    print(f'Раздел: {title}')                           # не понял
