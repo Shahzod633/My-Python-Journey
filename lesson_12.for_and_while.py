db4d59e989d495b66241ac071c2648100ac24e74
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-04-09 08:19:06 +0500

    Add functionality to find the two largest numbers from user input in lesson_12.py

--- lesson_12.for_and_while.py
+++ lesson_12.for_and_while.py
@@ -121,4 +121,18 @@ for j in range(1, n+1):
     else:
         Total+=j
 print(Total-total)
+
+# Наибольшое число 2 
+n=int(input())
+max1=0
+max2=0
+for j in range(n):
+    num=int(input())
+    if num>max1:
+        max2=max1
+        max1=num
+    elif num>max2:
+        max2=num
+print(max1)
+print(max2)
         
