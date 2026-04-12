04876560159f8c5356606c6fc25caed4fd3938b4
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-04-12 23:25:19 +0500

    Add additional nested loops examples for Таблица-1 and Таблица-2 in lesson_13

--- Nested_Loops_lesson_13.py
+++ Nested_Loops_lesson_13.py
@@ -3,3 +3,20 @@ for hours in range(24):
     for minutes in range(60):
         for seconds in range(60):
             print(hours, ':', minutes, ':', seconds)
+
+ # Таблица-1
+a=int(input())
+for i in range(a):
+    for j in range(3):
+        print(a, end=' ')
+    print()
+
+# Таблица-2 
+a=int(input())
+total=0
+for i in range(1,a+1):
+  for j in range(1, 6):
+    print(i, end=" ") 
+  print()
+
+
