35dcf821d6f3bec469f257019d14acd7fdaf1c5f
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-04-12 23:26:00 +0500

    Add Таблица-3 to demonstrate nested loops for addition in lesson_13

--- Nested_Loops_lesson_13.py
+++ Nested_Loops_lesson_13.py
@@ -19,4 +19,10 @@ for i in range(1,a+1):
     print(i, end=" ") 
   print()
 
+# Таблица-3
+a=int(input())
+for i in range(1, a+1):
+  for j in range(1, 10):
+    print(i, '+', j, '=', i+j)
+  print()
 
