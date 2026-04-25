f09abab0dab1caf80b131c608db9bb3ea837dc13
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-04-26 00:24:28 +0500

    Add functionality to create and print a specific concatenated list using multiplication and addition of existing lists

--- Lesson_15_Lists.py
+++ Lesson_15_Lists.py
@@ -37,3 +37,9 @@ print(rainbow)
 languages = ['Chinese', 'Spanish', 'English', 'Hindi', 'Arabic', 'Bengali', 'Portuguese', 'Russian', 'Japanese', 'Lahnda']
 print(languages[::-1])
 
+# Используя операторы конкатенации (+) и умножения списка на число (*), дополните приведённый ниже код так, чтобы он вывел следующий список:  [1, 2, 3, 1, 2, 3, 6, 6, 6, 6, 6, 6, 6, 6, 6, 7, 8, 9, 10, 11, 12, 13]
+numbers1 = [1, 2, 3]
+numbers2 = [6]
+numbers3 = [7, 8, 9, 10, 11, 12, 13]
+
+print((numbers1*2)+(numbers2*9)+(numbers3))
