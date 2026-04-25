7029ed8ca7acb61d4762672063cebbdb2a016dfd
Shahzod633 <Shahzodisorbon633@gmail.com>
2026-04-26 00:16:22 +0500

    Add functionality to calculate and print the average of a predefined list of even numbers

--- Lesson_15_Lists.py
+++ Lesson_15_Lists.py
@@ -20,4 +20,10 @@ Alist = [a[j] for j in range(0, len(a), 2)]
 
 print(Alist)
 
+# Дополните приведённый ниже код так, чтобы он вывел среднее арифметическое элементов списка evens
+evens = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+average =sum(evens)/len(evens)
+
+print(average)
+
 
